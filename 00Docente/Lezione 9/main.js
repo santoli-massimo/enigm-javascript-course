ff5d67f7da33eca5d257232a9a4f97dcd5a59e9f
santoli-massimo <santoli.massimo@gmail.com>
2023-02-14 09:55:20 +0100

    esercizio lezione 9

---
+++ 00Docente/Lezione 9/main.js
@@ -0,0 +1,14 @@
+console.log('Lezione 9')
+
+// ESERCIZIO
+// Loggare in console tutti gli elementi di un array
+// 1 elemento per riga
+// se order è minore di 0 loggare gli elementi in ordine inverso
+// rendere il valore predefinito di order uguale a 0
+
+function logList(list, order){
+
+    for(let i=0; i<list.length; i++){
+        console.log(list[i])
+    }
+}
