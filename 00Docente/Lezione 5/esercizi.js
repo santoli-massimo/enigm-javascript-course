92680ac7a3d8aab54bbdef12b2867638138fba07
santoli-massimo <santoli.massimo@gmail.com>
2023-02-14 09:54:59 +0100

    esercizio lezione 9

--- 00Docente/Lezione 5/esercizi.js
+++ 00Docente/Lezione 5/esercizi.js
@@ -34,5 +34,27 @@ function fullName(nome, cognome, regione){
         'Crownlands': 'Waters',
         'undefined': 'Doe'
     }
+    if(nome === undefined || nome === null) nome = 'John'
+    if(cognome === undefined || cognome === null){
+        // Regione non è definito
+        if(regione === undefined || regione === null){
+            cognome = 'Doe'
+        }
+        else{ // REgione è definito
+            let bastardName = bastardNamesByRegion[regione]
+            if(bastardName===undefined){ // La regione non esiste
+                cognome = 'Doe'
+            }
+            else{ // La regione esiste
+                cognome = bastardName
+            }
+        }
+    }
+
     return nome + ' ' + cognome
 }
+// let x = fullName()
+// let x = fullName(null, null, 'North')
+// let x = fullName(null, 'Wayne')
+let x = fullName('Bruce', 'Wayne')
+console.log(x)
