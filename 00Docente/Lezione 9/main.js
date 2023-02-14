4b96b372f33fbebc29c8cf5203c9accfffd1119f
santoli-massimo <santoli.massimo@gmail.com>
2023-02-14 10:04:59 +0100

    Lezione 9: aggiunti esempi all'esercizio

--- 00Docente/Lezione 9/main.js
+++ 00Docente/Lezione 9/main.js
@@ -6,9 +6,27 @@ console.log('Lezione 9')
 // se order è minore di 0 loggare gli elementi in ordine inverso
 // rendere il valore predefinito di order uguale a 0
 
+// ESEMPIO 1
+// logList([1,2,3])
+//  1
+//  2
+//  3
+
+// ESEMPIO 2
+// logList([1,2,3], -1)
+//  3
+//  2
+//  1
+
+// ESEMPIO 3
+// logList() non logga niente
+
 function logList(list, order){
 
     for(let i=0; i<list.length; i++){
         console.log(list[i])
     }
 }
+
+
+// logList([1,2,3])
